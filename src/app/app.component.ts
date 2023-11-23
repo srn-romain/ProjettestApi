f96795656e178ae75d1a8bf4968e2e105c0a5b38
srn-romain <Sieurinromain@gmail.com>
2023-11-23 17:53:57 +0100

    V2- Projet Fonctionnelle !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {Affichage2Component} from "./affichage2/affichage2.component";
+import {FormComponent} from "./form/form.component";
+import {Affichage1Component} from "./affichage1/affichage1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Affichage2Component, FormComponent, Affichage1Component],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
